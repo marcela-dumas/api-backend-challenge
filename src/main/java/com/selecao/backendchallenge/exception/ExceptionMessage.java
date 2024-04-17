0f3d490eaf65ab1731696f4b7f30f201005ad10b
marcela-dumas <marcela_dumas@hotmail.com>
2024-04-17 14:26:43 -0300

    corrigindo mensagens de erro

--- src/main/java/com/selecao/backendchallenge/exception/ExceptionMessage.java
+++ src/main/java/com/selecao/backendchallenge/exception/ExceptionMessage.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 public enum ExceptionMessage {
     INVALID_INSURANCE_CATEGORY("A categoria de seguro informada é inválida"),
-    INSURANCE_WITH_THIS_NAME_EXISTS("Já existe um seguro cadastrado com esse name"),
+    INSURANCE_WITH_THIS_NAME_EXISTS("Já existe um seguro cadastrado com esse nome"),
     INSURANCE_NOT_FOUND("Seguro não encontrado");
 
     private String message;
